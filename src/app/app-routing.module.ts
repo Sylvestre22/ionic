2e269bba007be0918d323ed74e90d1cdd65f463a
lyknowledge <mike.sylvestre@lyknowledge.io>
2021-03-29 10:52:11 +0200

    update page home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,38 +1,42 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [
-  {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  },
-  {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
-  },
-  {
-    path: 'login',
-    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
-  },
-  {
-    path: 'register',
-    loadChildren: () => import('./pages/register/register.module').then( m => m.RegisterPageModule)
-  },
-  {
-    path: 'profil',
-    loadChildren: () => import('./pages/profil/profil.module').then( m => m.ProfilPageModule)
-  },
-  {
-    path: 'search',
-    loadChildren: () => import('./pages/search/search.module').then( m => m.SearchPageModule)
-  },
+const routes: Routes = [{
+        path: '',
+        redirectTo: 'login',
+        pathMatch: 'full'
+    },
+    {
+        path: 'home',
+        loadChildren: () =>
+            import ('./home/home.module').then(m => m.HomePageModule)
+    },
+    {
+        path: 'login',
+        loadChildren: () =>
+            import ('./pages/login/login.module').then(m => m.LoginPageModule)
+    },
+    {
+        path: 'register',
+        loadChildren: () =>
+            import ('./pages/register/register.module').then(m => m.RegisterPageModule)
+    },
+    {
+        path: 'profil',
+        loadChildren: () =>
+            import ('./pages/profil/profil.module').then(m => m.ProfilPageModule)
+    },
+    {
+        path: 'search',
+        loadChildren: () =>
+            import ('./pages/search/search.module').then(m => m.SearchPageModule)
+    },
 ];
 
 @NgModule({
-  imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
-  ],
-  exports: [RouterModule]
+    imports: [
+        RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+    ],
+    exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
