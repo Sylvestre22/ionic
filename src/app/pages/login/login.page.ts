43ac8a1048c9a33d032b9f9003650164c4658363
lyknowledge <mike.sylvestre@lyknowledge.io>
2021-03-29 16:53:25 +0200

    login

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+
+import { Router } from '@angular/router';
+import { LoadingController } from '@ionic/angular';
+
 import { AuthService } from '../../services/auth.service';
-
-
 
 @Component({
     selector: 'app-login',
@@ -13,15 +15,22 @@ export class LoginPage implements OnInit {
     email: string = '';
     pass: string = '';
 
-    constructor(private auth: AuthService) {}
+    constructor(private router: Router, private auth: AuthService, private loading: LoadingController) {}
 
     ngOnInit() {}
 
-    loginForm() {
+    async loginForm() {
+        const load = await this.loading.create({
+            cssClass: 'my-custom-class',
+            message: 'Please wait...',
+        });
+        // await load.present();
         console.log(this.email);
         console.log(this.pass);
-        this.auth.getProfile().subscribe(data => {
-            console.log(data);
+        this.auth.getProfile().subscribe(async(user) => {
+            console.log(user);
+            this.router.navigate(['/home'])
+            await load.onDidDismiss();
         })
     }
 
