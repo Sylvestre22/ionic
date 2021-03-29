3cec847441d7a300f9fa54d46ed2d2c3d4696288
lyknowledge <mike.sylvestre@lyknowledge.io>
2021-03-29 14:26:33 +0200

    auth service

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class AuthService {
+
+    url: string = 'http://localhost:3000';
+
+    constructor(private http: HttpClient) {}
+
+    getProfile() {
+        return this.http.get(this.url + '/profil');
+    }
+}
