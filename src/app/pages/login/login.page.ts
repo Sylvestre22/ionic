548beb576b571d092406d985ce3f0ee345a4b569
lyknowledge <mike.sylvestre@lyknowledge.io>
2021-03-29 14:21:43 +0200

    auth service

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+
+
 
 @Component({
     selector: 'app-login',
@@ -10,13 +13,16 @@ export class LoginPage implements OnInit {
     email: string = '';
     pass: string = '';
 
-    constructor() {}
+    constructor(private auth: AuthService) {}
 
     ngOnInit() {}
 
     loginForm() {
         console.log(this.email);
         console.log(this.pass);
+        this.auth.getProfile().subscribe(data => {
+            console.log(data);
+        })
     }
 
 }
