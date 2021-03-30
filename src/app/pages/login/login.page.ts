a63f1e536684c2eb7afff447372d1d63ed9871ea
lyknowledge <mike.sylvestre@lyknowledge.io>
2021-03-30 10:11:55 +0200

    update login

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
 import { Router } from '@angular/router';
+import { ModalController } from '@ionic/angular';
 import { LoadingController } from '@ionic/angular';
 
 import { AuthService } from '../../services/auth.service';
+import { ForgotPasswordComponent } from '../../modals/forgot-password/forgot-password.component';
 
 @Component({
     selector: 'app-login',
@@ -15,9 +17,26 @@ export class LoginPage implements OnInit {
     email: string = '';
     pass: string = '';
 
-    constructor(private router: Router, private auth: AuthService, private loading: LoadingController) {}
+    isErrorMail: boolean = true;
+
+    constructor(private modal: ModalController, private router: Router, private auth: AuthService, private loading: LoadingController) {}
 
     ngOnInit() {}
+
+
+
+    async forgotPassword() {
+        const modal = await this.modal.create({
+            component: ForgotPasswordComponent,
+        });
+        return await modal.present();
+    }
+
+    checkEmail() {
+        const regex = new RegExp(/[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/g);
+        // this.isErrorMail = !regex.test(this.email);
+        this.isErrorMail = (regex.test(this.email.trim())) ? false : true;
+    }
 
     async loginForm() {
         const load = await this.loading.create({
@@ -30,7 +49,9 @@ export class LoginPage implements OnInit {
         this.auth.getProfile().subscribe(async(user) => {
             console.log(user);
             this.router.navigate(['/home'])
-            await load.onDidDismiss();
+                // await load.onDidDismiss();
+
+            await this.loading.dismiss();
         })
     }
 
