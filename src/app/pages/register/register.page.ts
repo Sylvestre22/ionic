48eb70eed84ed1925f0fbbfc8ecf9f4006465412
lyknowledge <mike.sylvestre@lyknowledge.io>
2021-03-30 14:14:51 +0200

    update page register

--- src/app/pages/register/register.page.ts
+++ src/app/pages/register/register.page.ts
@@ -1,15 +1,46 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
+
+interface userRegister {
+    firstname: string;
+    lastname: string;
+    phone: string;
+    email: string;
+    password: string;
+    repassword: string;
+}
+
 @Component({
-  selector: 'app-register',
-  templateUrl: './register.page.html',
-  styleUrls: ['./register.page.scss'],
+    selector: 'app-register',
+    templateUrl: './register.page.html',
+    styleUrls: ['../login/login.page.scss', './register.page.scss'],
 })
 export class RegisterPage implements OnInit {
 
-  constructor() { }
+    user: userRegister = { firstname: '', lastname: '', email: '', phone: '', password: '', repassword: '' };
 
-  ngOnInit() {
-  }
+    constructor(private camera: Camera) {}
+
+    ngOnInit() {}
+
+    uploadPicture() {
+        const options: CameraOptions = {
+            quality: 100,
+            destinationType: this.camera.DestinationType.FILE_URI,
+            encodingType: this.camera.EncodingType.JPEG,
+            mediaType: this.camera.MediaType.PICTURE
+        }
+
+        this.camera.getPicture(options).then((imageData) => {
+            // imageData is either a base64 encoded string or a file URI
+            // If it's base64 (DATA_URL):
+            let base64Image = 'data:image/jpeg;base64,' + imageData;
+            alert(imageData)
+        }, (err) => {
+            // Handle error
+            alert(err)
+        });
+    }
 
 }
